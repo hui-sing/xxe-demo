c621a1da33ecd57c54150ac6854578e4597c5d73
hui-sing <110897397+hui-sing@users.noreply.github.com>
2022-08-09 17:44:42 +0800

    Update xxe_test_demo.php

--- xxe_test_demo.php
+++ xxe_test_demo.php
@@ -1,3 +1,7 @@
+<html>
+<body>
+
+<h1> test begin </h1>
 
 <?php
 libxml_disable_entity_loader (false);
@@ -5,7 +9,8 @@ $xmlfile = file_get_contents('php://input');
 $dom = new DOMDocument();
 $dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
 $creds = simplexml_import_dom($dom);
-
-header('Content-Type: text/html; charset=utf-8');
 echo $creds;
 ?>
+
+</body>
+</html>
