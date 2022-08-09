ac7a95064e5951d21432b61df1ed60ff4931e3f3
hui-sing <110897397+hui-sing@users.noreply.github.com>
2022-08-09 17:13:27 +0800

    Add files via upload

---
+++ xxe_test_demo.php
@@ -0,0 +1,11 @@
+
+<?php
+libxml_disable_entity_loader (false);
+$xmlfile = file_get_contents('php://input');
+$dom = new DOMDocument();
+$dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+$creds = simplexml_import_dom($dom);
+
+header('Content-Type: text/html; charset=utf-8');
+echo $creds;
+?>
